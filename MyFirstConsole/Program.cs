f4d112f06f234036c1fde40ca27b76f4d616d19b
El Joshe <olivermen69@hotmail.com>
2019-11-26 21:59:24 -0500

    Add variables

--- MyFirstConsole/Program.cs
+++ MyFirstConsole/Program.cs
@@ -10,6 +10,29 @@ namespace MyFirstConsole
     {
         static void Main(string[] args)
         {
+            int x = 5;
+            float y = 4.5f;
+            string str = "lorem ipsum";
+            bool myBool = false;
+
+            float result = 0;
+
+            result = 5 + 7;
+            Console.WriteLine("suma: " + result);
+
+            result = 5 - 7;
+            Console.WriteLine("resta: " + result);
+
+            result = 5 * 7;
+            Console.WriteLine("multi: " + result);
+
+            result = 5f / 7f;
+            Console.WriteLine("divid: " + result);
+
+            result = 5 % 2;
+            Console.WriteLine("modul: " + result);
+
+            Console.ReadLine();
         }
     }
 }
